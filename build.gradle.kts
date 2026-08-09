73b7e5de38153cbe7bcaff29d77d914a4f4472f5
abcdeghi <semin4599@naver.com>
2026-08-09 16:23:54 +0900

    MVC 파일 구조로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,8 @@ dependencies {
     testAnnotationProcessor("org.projectlombok:lombok")
     testImplementation ("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly ("org.junit.platform:junit-platform-launcher")
+    implementation ("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation ("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect")
 }
 
 tasks.withType<Test> {
--- src/main/java/com/back/jumptospring/MainController.java
+++ src/main/java/com/back/jumptospring/MainController.java
@@ -12,4 +12,9 @@ public class MainController {
     public String index() {
         return "안녕하세요 스프링부트";
     }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
 }
