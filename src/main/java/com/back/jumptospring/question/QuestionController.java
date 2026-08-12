41a9d1961e7c597730dfe622004968f52dcaf9fe
abcdeghi <semin4599@naver.com>
2026-08-12 17:26:43 +0900

    수정 기능 추가

--- src/main/java/com/back/jumptospring/question/QuestionController.java
+++ src/main/java/com/back/jumptospring/question/QuestionController.java
@@ -6,11 +6,13 @@ import com.back.jumptospring.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 import java.util.List;
@@ -57,5 +59,22 @@ public class QuestionController {
         SiteUser siteUser= this.userService.getUser(principal.getName());
         this.questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String questionModify(@Valid QuestionForm questionForm
+            , BindingResult bindingResult
+            , @PathVariable("id") Integer id
+            , Principal principal) {
+        if(bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        Question question = this.questionService.getQuestion(id);
+        if(!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+        return String.format("redirect:/question/detail/%s", id);
     }
 }
